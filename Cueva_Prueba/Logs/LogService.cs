aa74f230dede607c54ed7614dcec2d02c0336bbd
Esteban1211 <spellman3@live.com>
2025-07-10 09:38:35 -0400

    Codido Finalizado,Ademas de correccion de errores: -Se agrego la carpeta de Logs -Se elimino del gitignore todo lo relacionado con logs/.logs

---
+++ Cueva_Prueba/Logs/LogService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cueva_Prueba.Logs
+{
+    public static class LogService
+    {
+        public static void AppendLog(string nombreReceta)
+        {
+            string path = Path.Combine(FileSystem.AppDataDirectory, "Logs_Cueva.txt");
+            string log = $"Se incluyó el registro {nombreReceta} el {DateTime.Now:dd/MM/yyyy HH:mm}\n";
+            File.AppendAllText(path, log);
+        }
+
+        public static string LeerLogs()
+        {
+            string path = Path.Combine(FileSystem.AppDataDirectory, "Logs_Cueva.txt");
+            return File.Exists(path) ? File.ReadAllText(path) : "No hay registros.";
+        }
+    }
+}
---
+++ Cueva_Prueba/Logs/RecetaEvents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cueva_Prueba.Logs
+{
+    public static class RecetaEvents
+    {
+        public static event Action? RecetaGuardada;
+
+        public static void OnRecetaGuardada()
+        {
+            RecetaGuardada?.Invoke();
+        }
+    }
+}
